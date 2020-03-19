60d438e7ad8234b37c644d70165828f9f751671b
dereSundayT <sundaytoluwanidere@gmail.com>
2020-03-19 11:25:29 +0000

    another commit

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,14 +8,14 @@ const Contact = () => {
 	<div className="row py-5">
             <div className="col-md-12">
             <small id="emailHelp" className="form-text text-muted text-center ">contact us</small>
-                <h1 className="text-purple"><b>Have an awesome idea in your mind?</b></h1>
-                <h1 className="text-purple"><b>we would love to hear</b></h1>
+                <h3 className="text-purple"><b>Have an awesome idea in your mind?</b></h3>
+                <h3 className="text-purple"><b>we would love to hear</b></h3>
                
                  <form className="form-inline">
                       <div className=" col-4 form-group  myInput">
                         <input type="email" className="form-control"  placeholder="enter your email" />
                       </div>
-                      <button type="submit" className="col-2 float-left" style={{width:'50px',marginRight:'20px',background:'blue',borderRadius:'20px', padding:'5px',color:'whitesmoke'}}>Start</button>
+                      <button type="submit" className="col-4 float-left" style={{marginRight:'20px',background:'blue',borderRadius:'20px', padding:'5px',color:'whitesmoke'}}>Start</button>
                 </form>
                                
             </div>
